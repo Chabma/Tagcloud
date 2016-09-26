ca46e4a305bad2e14a56484c2a984d3e8a96f91b
Chabma <chabma@gmail.com>
2016-09-26 13:50:07 -0400

    changeeed d3 request

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ input[type=datetime-local]{
 <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
 <script src="./jquery-3.1.1.min.js"></script>
-<script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+<script src="https://d3js.org/d3.v3.min.js" charset="utf-8"></script>
 <script src="./d3/d3.js"></script>
 <script src="./d3.layout.cloud.js"></script>
 <script type="text/javascript" src="html2canvas.js"></script>
